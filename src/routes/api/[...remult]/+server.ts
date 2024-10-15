c5f4c04022b5b0d9547c1ea63230b91d76584b51
Fabrice <fabrice.marguerie@gmail.com>
2024-10-15 16:38:26 +0200

    set json file path

--- src/routes/api/[...remult]/+server.ts
+++ src/routes/api/[...remult]/+server.ts
@@ -1,15 +1,19 @@
 import { remultSveltekit } from 'remult/remult-sveltekit'
 import { Job } from '../../../shared/Job'
-import { SqlDatabase } from 'remult'
-import sqlite3 from 'sqlite3'
-import { Sqlite3DataProvider } from 'remult/remult-sqlite3'
+// import { SqlDatabase } from 'remult'
+// import sqlite3 from 'sqlite3'
+// import { Sqlite3DataProvider } from 'remult/remult-sqlite3'
+import { JsonDataProvider } from "remult"
+import { JsonEntityFileStorage } from "remult/server"
 
 export const _api = remultSveltekit({
   entities: [Job],
   admin: true,
-  dataProvider: new SqlDatabase(
-    new Sqlite3DataProvider(new sqlite3.Database('./jobs.sqlite')),
-  ),
+  // dataProvider: new SqlDatabase(
+  //   new Sqlite3DataProvider(new sqlite3.Database('./jobs.sqlite')),
+  // ),
+  dataProvider: async () =>
+    new JsonDataProvider(new JsonEntityFileStorage("/tmp"))
 })
 
 export const GET = _api.GET
