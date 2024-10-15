dff88dead07478b456e54937688567e358a9d082
Fabrice <fabrice.marguerie@gmail.com>
2024-10-15 16:44:36 +0200

    data provider depending on mode

--- src/routes/api/[...remult]/+server.ts
+++ src/routes/api/[...remult]/+server.ts
@@ -1,19 +1,17 @@
+import { dev } from '$app/environment'
+import { JsonDataProvider, SqlDatabase } from 'remult'
+import { Sqlite3DataProvider } from 'remult/remult-sqlite3'
 import { remultSveltekit } from 'remult/remult-sveltekit'
-import { Job } from '../../../shared/Job'
-// import { SqlDatabase } from 'remult'
-// import sqlite3 from 'sqlite3'
-// import { Sqlite3DataProvider } from 'remult/remult-sqlite3'
-import { JsonDataProvider } from "remult"
 import { JsonEntityFileStorage } from "remult/server"
+import sqlite3 from 'sqlite3'
+import { Job } from '../../../shared/Job'
 
 export const _api = remultSveltekit({
   entities: [Job],
   admin: true,
-  // dataProvider: new SqlDatabase(
-  //   new Sqlite3DataProvider(new sqlite3.Database('./jobs.sqlite')),
-  // ),
-  dataProvider: async () =>
-    new JsonDataProvider(new JsonEntityFileStorage("/tmp"))
+  dataProvider: dev ? new SqlDatabase(
+    new Sqlite3DataProvider(new sqlite3.Database('./jobs.sqlite')),
+  ) : new JsonDataProvider(new JsonEntityFileStorage("/tmp"))
 })
 
 export const GET = _api.GET
